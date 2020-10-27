771cbe678d72169a9909e8f41e4c18b387ed18d4
fzh <stanfanass@163.com>
2020-10-27 10:22:30 +0800

    ymal读取

--- src/test/java/com/glaway/myspringboot/ymal/PersonTest.java
+++ src/test/java/com/glaway/myspringboot/ymal/PersonTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,11 +12,31 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @Slf4j
 public class PersonTest {
+
 	@Autowired
 	private Person person;
+
+	/**
+	 * @author 		   FZH
+	 * @date 		   2020/10/27
+	 * @parameter 	   []
+	 * @return 		   void
+	 * @description
+	 *      封装ymal数据导javabean
+	 **/
 	@Test
-	public void readYml() {
-		log.info("yml中person数据==>" + person.toString());
+	public void readYaml() {
+		log.info("yaml中person数据==>" + person.toString());
 	}
+
+
+	@Value("${person.dog.name}")
+	private String dogName;
+
+	@Test
+	public void readSignalYaml() {
+		log.info("yaml中单值==>" + dogName);
+	}
+
 
 }
