cda12880cd2817495423ed2222f689d172bc7665
fzh <stanfanass@163.com>
2020-10-27 09:48:01 +0800

    master

---
+++ src/main/java/com/glaway/myspringboot/ymal/Dog.java
@@ -0,0 +1,13 @@
+package com.glaway.myspringboot.ymal;
+/**
+ * @filename:       Gog
+ * @copyright:      版权所有 2020-2025 南京国睿信维软件有限公司
+ * @version:        V1.0
+ * @author:         FZH
+ * @createtime:     2020年10月27日9:35
+ * @description:   
+ *                      
+ */
+public class Gog {
+    
+}
---
+++ src/main/java/com/glaway/myspringboot/ymal/Person.java
@@ -0,0 +1,13 @@
+package com.glaway.myspringboot.ymal;
+/**
+ * @filename:       Person
+ * @copyright:      版权所有 2020-2025 南京国睿信维软件有限公司
+ * @version:        V1.0
+ * @author:         FZH
+ * @createtime:     2020年10月27日9:36
+ * @description:   
+ *                      
+ */
+public class Person {
+    
+}
